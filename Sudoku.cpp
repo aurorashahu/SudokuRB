0a82131b620ba835616fb858baac767b316fdf13
aurorashahu03 <aurorashahu03@gmail.com>
2025-03-11 16:20:11 -0400

    Adding back tracking counter. Replicating the backtracking function and adding a print statement to visually see the recursive backtracking and stating which value and its location was changed. Adding one easy and one unsolvable case to test functions.

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -5,6 +5,8 @@
 #include <cmath>
 using namespace std;
 
+int back_track_counter = 0; // Number of backtracks
+
 // Function to check if the input is valid
 bool valid_move(int array[9][9], int row, int col, int val) // Parameters the 2D array, row number, col number, and value we want to input
 {
@@ -34,43 +36,7 @@ bool valid_move(int array[9][9], int row, int col, int val) // Parameters the 2D
     return true; // Action is valid and passed all constraints
 }
 
-bool backtrack(int (&array)[9][9], int row, int col) // Pass array and current row and col position values
-{
-    // Solving row by row
-
-    if (row == 9) // if we finished processing the last row
-    {
-        return true;
-    }
-    else if (col == 9) // If we reached the last column in the row go on to the next
-    {
-        return backtrack(array, row + 1, 0); // Recursive call with updated row
-    }
-    else if (array[row][col] != 0) // If element is not empty in the puzzle
-    {
-        return backtrack(array, row, col + 1); // Recursive call with updated row
-    }
-    else // If empty space
-    {
-        for (int i = 1; i <= 9; i++) // loop and check which number 1-9 is valid in the space
-        {
-            if (valid_move(array, row, col, i)) // if valid
-            {
-                array[row][col] = i;                // place i there
-                if (backtrack(array, row, col + 1)) // solve the next space in the puzzle
-                {
-                    return true; // if successful solution return true
-                }
-                else
-                {
-                    array[row][col] = 0; // Backtrack, place zero back into the space to try another element
-                }
-            }
-        }
-        return false; // No solution
-    }
-}
-
+// FUNCTION TO PRINT CURRENT STATE OF PUZZLE
 void printSudoku(int array[9][9])
 {
     for (int i = 0; i < 9; i++)
@@ -91,6 +57,96 @@ void printSudoku(int array[9][9])
     }
 }
 
+/* ----------------- SOLVING GOING ROW BY ROW --------------------- */
+// FUNCTION WITHOUT PRINTING
+bool backtrack(int (&array)[9][9], int row, int col) // Pass array and current row and col position values
+{
+    // Solving row by row
+    if (row == 9) // if we finished processing the last row
+    {
+        cout << "Amount of times there was backtracking needed: " << back_track_counter << endl;
+        // reset counter for next time use of function
+        back_track_counter = 0;
+        return true;
+    }
+    else if (col == 9) // If we reached the last column in the row go on to the next
+    {
+        return backtrack(array, row + 1, 0); // Recursive call with updated row
+    }
+    else if (array[row][col] != 0) // If element is not empty in the puzzle
+    {
+        return backtrack(array, row, col + 1); // Recursive call with updated row
+    }
+    else // If
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            if (valid_move(array, row, col, i))
+            {
+                array[row][col] = i;
+                if (backtrack(array, row, col + 1))
+                {
+                    return true;
+                }
+                else
+                {
+                    array[row][col] = 0;
+                    // update coutner when a value was changed back to zero
+                    back_track_counter++;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+// FUNCTION WITH PRINTING TO VISUALIZE PROCESS
+bool backtrack_with_print(int (&array)[9][9], int row, int col) // Pass array and current row and col position values
+{
+    // Solving row by row
+    // update coutner
+    if (row == 9) // if we finished processing the last row
+    {
+        cout << "Amount of times there was backtracking needed: " << back_track_counter << endl;
+        // reset counter for next time use of function
+        back_track_counter = 0;
+        return true;
+    }
+    else if (col == 9) // If we reached the last column in the row go on to the next
+    {
+        return backtrack_with_print(array, row + 1, 0); // Recursive call with updated row
+    }
+    else if (array[row][col] != 0) // If element is not empty in the puzzle
+    {
+        return backtrack_with_print(array, row, col + 1); // Recursive call with updated row
+    }
+    else // If
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            if (valid_move(array, row, col, i))
+            {
+                array[row][col] = i;
+                cout << "\nAdding number " << i << " at (" << row << ", " << col << ")" << endl;
+                printSudoku(array);
+                if (backtrack_with_print(array, row, col + 1))
+                {
+                    return true;
+                }
+                else
+                {
+                    array[row][col] = 0;
+                    cout << endl;
+                    cout << "Backtracking at (" << row << ", " << col << ") - Removing " << i << endl;
+                    printSudoku(array);
+                    back_track_counter++;
+                }
+            }
+        }
+        return false;
+    }
+}
+
 int main()
 {
     int sudoku[9][9] = {
@@ -106,12 +162,18 @@ int main()
         {0, 3, 0, 0, 0, 7, 1, 0, 2},
         {9, 0, 2, 5, 0, 1, 0, 0, 0}};
 
-    cout < "PUZZLE 1";
+    cout << "----------------------------- PUZZLE 1---------------------------------" << endl;
     printSudoku(sudoku);
 
-    backtrack(sudoku, 0, 0);
-    cout < "------------------------- ";
-    printSudoku(sudoku);
+    if (!backtrack(sudoku, 0, 0))
+    {
+        cout << "No solution for this puzzle" << endl;
+    }
+    else
+    {
+        cout << "------------------------Solution----------------------------- \n";
+        printSudoku(sudoku);
+    }
 
     int sudoku2[9][9] = {
         {6, 8, 0, 0, 0, 0, 0, 5, 0},
@@ -126,12 +188,69 @@ int main()
         {0, 0, 0, 4, 0, 0, 0, 0, 0},
         {0, 3, 0, 0, 0, 0, 0, 2, 8}};
 
-    cout < "PUZZLE 2 ";
+    cout << "----------------------------------------- PUZZLE 2 --------------------- " << endl;
     printSudoku(sudoku2);
+    if (!backtrack(sudoku2, 0, 0))
+    {
+        cout << "No solution for this puzzle" << endl;
+    }
+    else
+    {
+        cout << "------------------------------------------Solution-------------------------------------- \n";
+        printSudoku(sudoku2);
+    }
 
-    backtrack(sudoku2, 0, 0);
-    cout < "------------------------- ";
-    printSudoku(sudoku2);
+    int sudoku3[9][9] = {
+        {4, 0, 2, 6, 3, 8, 1, 0, 0},
+        {0, 8, 0, 9, 5, 0, 3, 0, 0},
+        {3, 1, 0, 0, 7, 2, 5, 0, 6},
+
+        {0, 7, 0, 1, 2, 3, 6, 5, 0},
+        {8, 0, 0, 7, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 8, 0, 0, 0, 0},
+
+        {0, 4, 3, 8, 1, 0, 0, 0, 2},
+        {6, 0, 0, 0, 0, 4, 0, 1, 0},
+        {1, 9, 0, 0, 0, 5, 0, 0, 7}};
+
+    cout << "\n************** EASY PUZZLE TO SHOW MOVEMENTS ********************* " << endl;
+    printSudoku(sudoku3);
+
+    if (!backtrack_with_print(sudoku3, 0, 0))
+    {
+        cout << "No solution for this puzzle" << endl;
+    }
+    else
+    {
+        cout << "----------Solution------------- \n";
+        printSudoku(sudoku3);
+    }
+
+    int unsolvableSudoku[9][9] = {
+        {5, 1, 6, 8, 4, 9, 7, 3, 2},
+        {3, 0, 7, 6, 0, 5, 0, 0, 0},
+        {8, 0, 9, 7, 0, 0, 0, 6, 5},
+
+        {1, 3, 5, 0, 6, 0, 9, 0, 7},
+        {4, 7, 2, 5, 9, 1, 0, 0, 0},
+        {9, 6, 8, 3, 7, 0, 0, 5, 0},
+
+        {2, 5, 3, 1, 8, 6, 0, 7, 4},
+        {6, 8, 4, 9, 2, 7, 5, 0, 0},
+        {7, 9, 1, 0, 5, 0, 6, 8, 3} // 6 appears twice in column 9
+    };
+
+    cout << "\n~~~~~~~~~~~~~~~~~~~UNSOLVABLE PUZZLE ~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+    printSudoku(unsolvableSudoku);
+    if (!backtrack(unsolvableSudoku, 0, 0))
+    {
+        cout << "No solution for this puzzle" << endl;
+    }
+    else
+    {
+        cout << "------------------------------------------Solution-------------------------------------- \n";
+        printSudoku(unsolvableSudoku);
+    }
 
     return 0;
 }
